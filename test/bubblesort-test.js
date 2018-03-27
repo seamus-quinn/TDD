fab7488dd3e5e3cd305f8184d285ce4ce416eb63
Seamus Quinn <sumaesquinn@gmail.com>
2018-03-27 14:04:24 -0600

    Add array generator file and function

--- test/bubblesort-test.js
+++ test/bubblesort-test.js
@@ -3,12 +3,10 @@ const assert = chai.assert;
 const expect = chai.expect;
 const bubbleSort = require('./../lib/bubblesort')
 
+
 describe('Bubble Sort', function() {
   it('should be a function', function() {
     assert.isFunction(bubbleSort)
   });
 
-  it('should return an array', function() {
-    assert.equal()
-  })
 })
