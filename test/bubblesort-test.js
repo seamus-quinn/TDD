7f321490a10af3cbdbefd7d58ccfee7eb13aa5f1
Seamus Quinn <sumaesquinn@gmail.com>
2018-03-27 13:49:01 -0600

    Set up inital tests for bubblesort

---
+++ test/bubblesort-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const bubbleSort = require('./../lib/bubblesort')
+
+describe('Bubble Sort', function() {
+  it('should be a function', function() {
+    assert.isFunction(bubbleSort)
+  });
+
+  it('should return an array', function() {
+    assert.equal()
+  })
+})
